60f8852fbd0f1456885f75c123ccfca5a123801e
Ibraheem Khalifa <ibraheemkhalifa@gmail.com>
2021-06-09 07:50:17 -0400

    updated

--- workflows/relay.js
+++ workflows/relay.js
@@ -40,6 +40,7 @@ export default createWorkflow(relay => {
     if(notificationEvent.event === `ack_event`) {
       requests.shift()
       await relay.say(`${notificationEvent.source} is bringing your dog to the front.`)
+      await relay.cancelAlert('Pickup', notificationEvent.targets)
       await relay.broadcast(`You have accepted the current request. There are ${requests.length} requests pending.`, [`${notificationEvent.source}`])
       relay.terminate()
     }
