def7db241127df32f2f007f625debd2a59e140b3
shakeebshams <shakeebshams@gmail.com>
2021-07-30 14:52:48 -0400

    delays for async processing

--- workflows/dispatch.js
+++ workflows/dispatch.js
@@ -12,6 +12,7 @@ const createApp = (relay) => {
         await relay.flash(`FFA500`) //on initiation of workflow, start flashing LEDs orange
         await relay.say('Launching helicopter. Sending notification to Hiawatha Community Hospital')
         await relay.rotate(`00FF00`)
+        setTimeout(() => {  console.log("delay part 3"); }, 6000)
         console.log("dispatch: before emit")
         emitterDispatch.emit(`launch`, `20`)
         console.log("dispatch: after emit")
@@ -23,6 +24,7 @@ const createApp = (relay) => {
         if (button.button === `action`) {
             if (button.taps === `single`) {
                 await relay.say('sending ETA update to Hiawatha Community Hospital')
+                setTimeout(() => {  console.log("delay part 3"); }, 6000)
                 emitterDispatch.emit(`update`, `5`)
                 await relay.terminate()
             }
